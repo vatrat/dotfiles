6995394bf8b7f78be04fe822c31fa46f7d9b843d
vatrat <vatrat.dev@gmail.com>
2018-10-22 04:03:20 -0400

    Fix lock script typo

--- .xmonad/.xmonad_lock.sh
+++ .xmonad/.xmonad_lock.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 scrot /tmp/screen.png
-convert /tmp/screen.png -filter point -resize 20% -resize 1000% /tmp/screen.png
+convert /tmp/screen.png -filter point -resize 10% -resize 1000% /tmp/screen.png
 i3lock -i /tmp/screen.png
