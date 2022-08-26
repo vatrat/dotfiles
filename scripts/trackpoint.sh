c8960cb36a47d7f0f29e285831d04435e5f03f59
vatrat <vatrat.dev@gmail.com>
2022-08-26 15:55:51 -0500

    change mouse settings

--- scripts/trackpoint.sh
+++ scripts/trackpoint.sh
@@ -1,2 +1,2 @@
-echo 55 | sudo tee /sys/devices/platform/i8042/serio1/subsystem/devices/serio3/speed
-echo 250 | sudo tee /sys/devices/platform/i8042/serio1/subsystem/devices/serio3/sensitivity
+echo 220 | sudo tee /sys/devices/platform/i8042/serio1/subsystem/devices/serio2/speed
+echo 130 | sudo tee /sys/devices/platform/i8042/serio1/subsystem/devices/serio2/sensitivity
